51c2cc409cf83951937043a3ee775f5efa8ef930
Juliano Pereira Chagas <chagas.julianopereira@gmail.com>
2026-07-13 13:08:25 -0300

    arquivos

---
+++ src/Livro.java
@@ -0,0 +1,8 @@
+public class Livro {
+    private String titulo;
+    private String autor;
+    private String editora;
+    private int totalDePaginas;
+    private int paginaAtual;
+
+    }
---
+++ src/publicacao.java
@@ -0,0 +1,9 @@
+public interface publicacao {
+    public void abrir();
+    public void fechar();
+    public void avancarPag(int quant);
+    public void voltarPag(int quant);
+    public void avancarPag(int quant, double valor);
+    public void voltarPag(int quant, double valor);
+    public void folhear(int quant, double valor);
+}
