45d59bdec3ec514cb46d47da1ec046f8248bac75
JulianoPereiraChagas <chagas.julianopereira@gmail.com>
2026-07-14 14:35:58 -0300

    arquivos

--- src/ProjetoLivro.java
+++ src/ProjetoLivro.java
@@ -13,7 +13,10 @@ public class ProjetoLivro {
 
         l[0].abrir();
         l[0].folhear(200);
-
         System.out.println(l[0].detalhes());
+
+        l[1].abrir();
+        l[1].folhear(400);
+        System.out.println(l[1].detalhes());
     }
 }
